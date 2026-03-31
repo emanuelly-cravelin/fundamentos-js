4944c1e79836d8311ddf61756a3e16db2cceebba
emanuelly-cravelin <emanuelly.cravelin@escola.pr.gov.br>
2026-03-31 15:06:09 +0000

    argumentos

--- parametros.js
+++ parametros.js
@@ -11,3 +11,9 @@ function nomeIdade(nome, idade){
 }
 
 console.log(nomeIdade("Emanuelly", 16))
+
+function multiplica(numero1, numero2){
+    return numero1 * numero2;
+}
+
+console.log(multiplica(soma(4, 5), soma(3, 3)))
